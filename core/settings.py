3d3891ba075c0d12fa7b8f05143353014e16265b
Anwar Hossain <mahedianwar@gmail.com>
2023-10-31 22:33:17 +0600

    created custom user model

--- core/settings.py
+++ core/settings.py
@@ -156,3 +156,5 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.UserAccount'
